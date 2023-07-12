38bf34fe109ace210e65ce6e05e2bd128c7749f9
vinaymanchanda <vmanchanda4@gmail.com>
2023-07-12 14:33:36 +0530

    obj

--- src/components/container.js
+++ src/components/container.js
@@ -5,7 +5,20 @@ const Container = () => {
   const [click, setClick] = useState(false);
   const videoRef = useRef(null);
 
- 
+  const mediaArray = [
+    {
+      type: "video",
+      url: " https://www.youtube.com/watch?v=a3ICNMQW7Ok&ab_channel=TimotiusJoso",
+    },
+    {
+      type: "gif",
+      url: "https://infowithart.com/wp-content/uploads/2019/01/Cover-image.gif",
+    },
+    {
+      type: "image",
+      url: "https://infowithart.com/wp-content/uploads/2019/01/Cover-image.gif",
+    },
+  ];
 
   useEffect(() => {
     // Autoplay the video on component mount
@@ -15,73 +28,56 @@ const Container = () => {
   }, []);
 
   return (
-    
-     //className="col-12 col-sm-6 col-md-4 col-lg-3 mt-5 "
-       
-    <div className="card border border-dark">
-    
-    <div className="d-flex justify-content-between">
-            <div>
-              {click ? (
-                <AiFillHeart
-                  size={22}
-                  className="cursor-pointer absolute right-2 top-5 "
-                  onClick={() => setClick(!click)}
-                  color={click ? "red" : "#333"}
-                  title="Remove from wishlist"
-                />
-              ) : (
-                <AiOutlineHeart
-                  size={22}
-                  className="cursor-pointer absolute right-2 top-5"
-                  onClick={() => setClick(!click)}
-                  color={click ? "red" : "#333"}
-                  title="Add to wishlist"
-                />
-              )}
-            </div>
-            <div>
-              <div className="dummy-title">Dummy Title</div>
-            </div>
-          </div>
-
-    <img
-             src="https://infowithart.com/wp-content/uploads/2019/01/Cover-image.gif"
-             className="rounded"
-             alt=""
-          />
+    <div className=" col-lg-7 m-2 p-3 card border border-dark">
+      <div className="d-flex justify-content-between">
+        <div>
+          {click ? (
+            <AiFillHeart
+              size={22}
+              className="cursor-pointer absolute right-2 top-5 "
+              onClick={() => setClick(!click)}
+              color={click ? "red" : "#333"}
+              title="Remove from wishlist"
+            />
+          ) : (
+            <AiOutlineHeart
+              size={22}
+              className="cursor-pointer absolute right-2 top-5"
+              onClick={() => setClick(!click)}
+              color={click ? "red" : "#333"}
+              title="Add to wishlist"
+            />
+          )}
         </div>
-        
+        <div>
+          <div className="dummy-title">Dummy Title</div>
+        </div>
+      </div>
 
-
-        //   {/* <div
-        //   className={`favorite-icon ${isFavorite ? "favorite" : ""}`}
-        //   onClick={handleFavoriteToggle}
-        // ></div> */}
-
-        //   {/* Media content (image, GIF, or video) goes here */}
-        //   {/*side options */}
-
-        //   {/* GIF
-        //  <img
-        //   src="https://infowithart.com/wp-content/uploads/2019/01/Cover-image.gif" 
-        //   alt="Example GIF"
-        //   className="gif"
-        // /> */}
-
-        //   {/* Video
-        // <video
-        //   src="https://www.youtube.com/watch?v=a3ICNMQW7Ok&ab_channel=TimotiusJoso"
-        //   ref={videoRef}
-        //   autoPlay
-        //   muted
-        //   loop
-        //   className="video"
-        // ></video> */}
-       
-        
-        
-    
+      <div>
+        {mediaArray.map((media, index) => {
+          if (media.type === "image") {
+            return <img src={media.url} alt='{`Image ${index}`} '/>;
+          } else if (media.type === "video") {
+            return (
+              <video
+                src={media.url}
+                controls
+                autoPlay
+                muted
+                key={`video-${index}`}
+              >
+                Your browser does not support the video tag.
+              </video>
+            );
+          } else if (media.type === "gif") {
+            return <img src={media.url} alt={`GIF ${index}`} />;
+          } else {
+            return null; // Invalid media type, you can handle it as per your requirement
+          }
+        })}
+      </div>
+    </div>
   );
 };
 
